52bb659c4b4bfaa742a2257035eac539badf9fbe
Quinn Joyce <62348805+qwage@users.noreply.github.com>
2023-06-11 23:34:17 -0700

    Revert "Assignment 6 Submit"

--- quinnsBlog/polling/urls.py
+++ quinnsBlog/polling/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from polling.views import PollListView, PollDetailView
+from polling.views import list_view, detail_view
 
 urlpatterns = [
-    path('', PollListView.as_view(), name='poll_index'),
-    path('polls/<int:pk>/', PollDetailView.as_view(), name='poll_detail'),
+    path('', list_view, name="poll_index"),
+    path('polls/<int:poll_id>/', detail_view, name="poll_detail"),
 ]
